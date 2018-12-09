8dd97d041d2c95da5ab0e39e979f8eb3d0c493e2
Tom Kealamakia <tkealamakia@gmail.com>
2018-12-09 06:30:57 -0700

    Adding model objects

---
+++ src/main/java/com/tsunazumi/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.tsunazumi.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/tsunazumi/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.tsunazumi.sfgpetclinic.model;
+
+public class Vet extends Person {
+}
