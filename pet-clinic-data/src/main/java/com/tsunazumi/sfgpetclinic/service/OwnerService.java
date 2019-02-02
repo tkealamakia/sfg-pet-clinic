b8fa78c4945df05c275632d41ed9fa0ee10e3911
Tom Kealamakia <tkealamakia@gmail.com>
2019-02-02 06:15:55 -0700

    Remove duplicate service methods

--- pet-clinic-data/src/main/java/com/tsunazumi/sfgpetclinic/service/OwnerService.java
+++ pet-clinic-data/src/main/java/com/tsunazumi/sfgpetclinic/service/OwnerService.java
@@ -2,16 +2,6 @@ package com.tsunazumi.sfgpetclinic.service;
 
 import com.tsunazumi.sfgpetclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
-
-  Owner findById(Long id);
-
-  Owner findByLastName(Long id);
-
-  Owner save(Owner owner);
-
-  Set<Owner> findAll();
+public interface OwnerService extends CrudService<Owner, Long>{
 
 }
--- pet-clinic-data/src/main/java/com/tsunazumi/sfgpetclinic/service/VetService.java
+++ pet-clinic-data/src/main/java/com/tsunazumi/sfgpetclinic/service/VetService.java
@@ -1,16 +1,8 @@
 package com.tsunazumi.sfgpetclinic.service;
 
-import com.tsunazumi.sfgpetclinic.model.Pet;
 import com.tsunazumi.sfgpetclinic.model.Vet;
 
-import java.util.Set;
 
-public interface VetService {
-
-  Vet findById(Long id);
-
-  Vet save(Vet vet);
-
-  Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 
 }
